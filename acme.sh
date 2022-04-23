a85a0c9a72daf27ed2f8dadf1ede701d89cd9528
source <source@my.domain>
2022-04-23 15:32:53 +0200

    updated themes, added parse_open.sh

--- acme.sh
+++ acme.sh
@@ -10,8 +10,9 @@ CGO_LDFLAGS="-L/usr/local/lib"
 MANPATH=$PLAN9/man export MANPATH
 PATH=${PLAN9}/bin:$PATH export PATH
 
+pgrep plumber >/dev/null || plumber
+
 BUTMOD=0x4c7899ff
-SCRBAR=0x333333ff
 
 TXTFG=0xd0d0d0ff
 TXTBG=0x1c1c1cff
@@ -29,33 +30,38 @@ TAGHLBG=$TAGFG
 
 B2HL=0x400000ff
 B3HL=0x004000ff
-. ./solarized.theme
 
-TEMPLATE=$(printf '%s' \
-	"textback:$TXTBG,"\
-	"texthigh:$TXTHLBG,"\
-	"textbord:$TXTBORD,"\
-	"texttext:$TXTFG,"\
-	"texthtext:$TXTHLFG,"\
-	"tagback:$TAGBG,"\
-	"taghigh:$TAGHLBG,"\
-	"tagbord:$TAGBORD,"\
-	"tagtext:$TAGFG,"\
-	"taghtext:$TAGHLFG,"\
-	"butmod:$BUTMOD,"\
-	"butcol:$BUTCOL,"\
-	"but2col:$B2HL,"\
-	"but3col:$B3HL")
+if [ "${0##*/}" = "acmebright" ]; then
+. /home/fabian/src/acme-scripts/solarized-bright.theme
+fi
 
-pgrep plumber >/dev/null || plumber
+if [ "${0##*/}" = "acmedark" ]; then
+. /home/fabian/src/acme-scripts/solarized-dark.theme
+fi
 
 #	-f /mnt/font/GoMono/10a/font
 
-if [ "${0##*/}" = "acmedark" ]; then
+if [ "${0##*/}" = "acmedark" ] || [ "${0##*/}" = "acmebright" ]; then
+	TEMPLATE=$(printf '%s' \
+		"textback:$TXTBG,"\
+		"texthigh:$TXTHLBG,"\
+		"textbord:$TXTBORD,"\
+		"texttext:$TXTFG,"\
+		"texthtext:$TXTHLFG,"\
+		"tagback:$TAGBG,"\
+		"taghigh:$TAGHLBG,"\
+		"tagbord:$TAGBORD,"\
+		"tagtext:$TAGFG,"\
+		"taghtext:$TAGHLFG,"\
+		"butmod:$BUTMOD,"\
+		"butcol:$BUTCOL,"\
+		"but2col:$B2HL,"\
+		"but3col:$B3HL")
+
 	SHELL=${PLAN9}/bin/rc $ACME -c 1 -a -M \
-		-f ${PLAN9}/font/fixed/unicode.9x15.font \
-		-t "$TEMPLATE" \
-		"$@" $ACME_AUX/guide
+			-f ${PLAN9}/font/fixed/unicode.9x15.font \
+			-t "$TEMPLATE" \
+			"$@" $ACME_AUX/guide
 else
 	SHELL=${PLAN9}/bin/rc $ACME -c 1 -a -M \
 		-f ${PLAN9}/font/fixed/unicode.9x15.font \
