e9456ff948c5098e1cd93e9a1aaccb1d1c8aa809
source <source@my.domain>
2022-04-25 20:45:33 +0200

    update theme path, cleanup

--- acme.sh
+++ acme.sh
@@ -2,44 +2,23 @@
 set -eu
 
 ACME=${PLAN9}/bin/acme
-ACME_AUX=$HOME/.acme
+ACME_AUX=${HOME}/.acme
 
-CGO_CFLAGS="-I/usr/local/include/"
-CGO_LDFLAGS="-L/usr/local/lib"
+# CGO_CFLAGS="-I/usr/local/include/"
+# CGO_LDFLAGS="-L/usr/local/lib"
 
 MANPATH=$PLAN9/man export MANPATH
 PATH=${PLAN9}/bin:$PATH export PATH
 
 pgrep plumber >/dev/null || plumber
 
-BUTMOD=0x4c7899ff
-
-TXTFG=0xd0d0d0ff
-TXTBG=0x1c1c1cff
-TXTBG=0x121212ff
-
-TXTHLFG=$TXTFG
-TXTHLBG=0x285577ff
-
-#TAGFG=0x9e9e9eff
-TAGFG=$BUTMOD
-TAGBG=0x121212ff
-TAGBG=0x1c1c1cff
-TAGHLFG=$TAGBG
-TAGHLBG=$TAGFG
-
-B2HL=0x400000ff
-B3HL=0x004000ff
-
 if [ "${0##*/}" = "acmebright" ]; then
-. /home/fabian/src/acme-scripts/solarized-bright.theme
+. ${ACME_AUX}/solarized-bright.theme
 fi
 
 if [ "${0##*/}" = "acmedark" ]; then
-. /home/fabian/src/acme-scripts/solarized-dark.theme
+. ${ACME_AUX}/solarized-dark.theme
 fi
-
-#	-f /mnt/font/GoMono/10a/font
 
 if [ "${0##*/}" = "acmedark" ] || [ "${0##*/}" = "acmebright" ]; then
 	TEMPLATE=$(printf '%s' \
