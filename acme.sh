97a8c3edcf520ef042e4315dc4bb96f1438cf345
user <user@my.domain>
2024-11-18 11:27:29 +0100

    update acme.sh, deps.sh

--- acme.sh
+++ acme.sh
@@ -1,23 +1,37 @@
 #!/bin/sh
 set -eu
 
+SCRIPT_DIR=$(dirname "$(readlink -f $0)")
+
+# Namespace superseeds DISPLAY
+NAMESPACE=/tmp/ns.acme.p9p export NAMESPACE
+mkdir -p "${NAMESPACE}"
+
 ACME=${PLAN9}/bin/acme
-ACME_AUX=${HOME}/.acme
+GUIDE=${SCRIPT_DIR}/guide
 
 # CGO_CFLAGS="-I/usr/local/include/"
 # CGO_LDFLAGS="-L/usr/local/lib"
 
 MANPATH=$PLAN9/man export MANPATH
-PATH=${PLAN9}/bin:$PATH export PATH
+# PATH=${PLAN9}/bin:$PATH
+PATH=$PATH:${PLAN9}/bin
+PATH=${SCRIPT_DIR}/cmd/:$PATH
+export PATH
 
-pgrep plumber >/dev/null || plumber
+pgrep -f -x "9pserve -u unix\!${NAMESPACE}/plumb" || \
+	plumber
+
+pgrep -f -x acmefocused || \
+	acmefocused &
+
 
 if [ "${0##*/}" = "acmebright" ]; then
-. ${ACME_AUX}/solarized-bright.theme
+. ${SCRIPT_DIR}/solarized-bright.theme
 fi
 
 if [ "${0##*/}" = "acmedark" ]; then
-. ${ACME_AUX}/solarized-dark.theme
+. ${SCRIPT_DIR}/solarized-dark.theme
 fi
 
 if [ "${0##*/}" = "acmedark" ] || [ "${0##*/}" = "acmebright" ]; then
@@ -37,12 +51,12 @@ if [ "${0##*/}" = "acmedark" ] || [ "${0##*/}" = "acmebright" ]; then
 		"but2col:$B2HL,"\
 		"but3col:$B3HL")
 
-	SHELL=${PLAN9}/bin/rc $ACME -c 1 -a -M \
-			-f ${PLAN9}/font/fixed/unicode.9x15.font \
+	SHELL=${PLAN9}/bin/rc $ACME -c 1 -a \
+			-f ${PLAN9}/font/terminus/unicode.18.font \
 			-t "$TEMPLATE" \
-			"$@" $ACME_AUX/guide
+			"$@" $GUIDE
 else
-	SHELL=${PLAN9}/bin/rc $ACME -c 1 -a -M \
-		-f ${PLAN9}/font/fixed/unicode.9x15.font \
-		"$@" $ACME_AUX/guide
+	SHELL=${PLAN9}/bin/rc $ACME -c 1 -a \
+		-f ${PLAN9}/font/terminus/unicode.18.font \
+		"$@" $GUIDE
 fi
--- deps.sh
+++ deps.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 # install various acme things
-go install 9fans.net/go/acme/acmego@latest
+# libX11-devel libXt-devel libXext-devel fontconfig-devel
+#go install 9fans.net/go/acme/acmego@latest
 go install golang.org/x/tools/cmd/goimports@latest
+GO111MODULE=on go install 9fans.net/acme-lsp/cmd/acme-lsp@latest
+GO111MODULE=on go install 9fans.net/acme-lsp/cmd/L@latest
+GO111MODULE=on go install 9fans.net/acme-lsp/cmd/acmefocused@latest
+
