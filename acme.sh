1aabe09f00c767fb6472de8adc5afef489694b7f
Your Name <you@example.com>
2025-07-06 12:43:28 +0200

    update acme.sh

--- acme.sh
+++ acme.sh
@@ -3,64 +3,41 @@ set -eu
 
 SCRIPT_DIR=$(dirname "$(readlink -f $0)")
 
+trap 'set +e; _cleanup; trap - EXIT; exit' \
+	EXIT INT HUP TERM
+
+_cleanup() {
+	pkill -x -f "${AUTO_RC}"
+	pkill acmefocused
+	pkill plumber;
+}
+
+command -v 9 && RUN9=9 || RUN9="${HOME}/src/plan9port/bin/9"
+
+PLAN9="${PLAN9:-${HOME}/src/plan9port}"
+export PLAN9
+
+PATH="${SCRIPT_DIR}/cmd:${PATH}"
+export PATH
+
+AUTO_RC="rc ${SCRIPT_DIR}/auto.rc"
+
 # Namespace superseeds DISPLAY
 NAMESPACE=/tmp/ns.acme.p9p export NAMESPACE
 mkdir -p "${NAMESPACE}"
 
-ACME=${PLAN9}/bin/acme
 GUIDE=${SCRIPT_DIR}/guide
 
-# CGO_CFLAGS="-I/usr/local/include/"
-# CGO_LDFLAGS="-L/usr/local/lib"
-
-MANPATH=$PLAN9/man export MANPATH
-# PATH=${PLAN9}/bin:$PATH
-PATH=$PATH:${PLAN9}/bin
-PATH=${SCRIPT_DIR}/cmd/:$PATH
-export PATH
-
 pgrep -f -x "9pserve -u unix\!${NAMESPACE}/plumb" || \
-	plumber
+	${RUN9} plumber
 
 pgrep -f -x acmefocused || \
 	acmefocused &
 
-cmd="rc ${SCRIPT_DIR}/auto.rc"
-pgrep -f -x "${cmd}" || \
-	$cmd &
+pgrep -f -x "${AUTO_RC}" || \
+	${RUN9} $AUTO_RC &
 
-
-if [ "${0##*/}" = "acmebright" ]; then
-. ${SCRIPT_DIR}/solarized-bright.theme
-fi
-
-if [ "${0##*/}" = "acmedark" ]; then
-. ${SCRIPT_DIR}/solarized-dark.theme
-fi
-
-if [ "${0##*/}" = "acmedark" ] || [ "${0##*/}" = "acmebright" ]; then
-	TEMPLATE=$(printf '%s' \
-		"textback:$TXTBG,"\
-		"texthigh:$TXTHLBG,"\
-		"textbord:$TXTBORD,"\
-		"texttext:$TXTFG,"\
-		"texthtext:$TXTHLFG,"\
-		"tagback:$TAGBG,"\
-		"taghigh:$TAGHLBG,"\
-		"tagbord:$TAGBORD,"\
-		"tagtext:$TAGFG,"\
-		"taghtext:$TAGHLFG,"\
-		"butmod:$BUTMOD,"\
-		"butcol:$BUTCOL,"\
-		"but2col:$B2HL,"\
-		"but3col:$B3HL")
-
-	SHELL=${PLAN9}/bin/rc $ACME -c 1 -a \
-			-f ${PLAN9}/font/fixed/unicode.9x18.font \
-			-t "$TEMPLATE" \
-			"$@" $GUIDE
-else
-	SHELL=${PLAN9}/bin/rc $ACME -c 1 -a \
-		-f ${PLAN9}/font/fixed/unicode.9x18.font \
-		"$@" $GUIDE
-fi
+#SHELL='/usr/bin/rc' ${RUN9} acme -c 1 -a \
+${RUN9} acme -c 1 -a \
+	-f ${PLAN9}/font/fixed/unicode.9x18.font \
+	"$@" $GUIDE
