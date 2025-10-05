e9e5d11d9fa7cf94657fc04a07a610eb6f50f064
Your Name <you@example.com>
2025-10-05 18:00:33 +0200

    add command to update menu

--- deps.sh
+++ deps.sh
@@ -3,6 +3,7 @@
 # libX11-devel libXt-devel libXext-devel fontconfig-devel
 #go install 9fans.net/go/acme/acmego@latest
 go install golang.org/x/tools/cmd/goimports@latest
+go install 9fans.net/go/acme/editinacme@latest
 GO111MODULE=on go install 9fans.net/acme-lsp/cmd/acme-lsp@latest
 GO111MODULE=on go install 9fans.net/acme-lsp/cmd/L@latest
 GO111MODULE=on go install 9fans.net/acme-lsp/cmd/acmefocused@latest
